70690fed082d5e1e9b12dc7db9fafa51388108df
oliver <oli.harboe@gmail.com>
2025-05-15 22:30:29 +0200

    first semi working version

---
+++ deckmaker.py
@@ -0,0 +1,117 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from random import randint
+from models import mistral_model
+from heapq import heappush,heappop
+
+
+
+class FlashcardDeck:
+    def __init__(self):
+        self.cards = {} # cards with question, answer and context
+        self.path = "books/" # path to pdfs 
+        self.chunks = [] # chunks of pdf
+        self.cardheap = [] # priority queue of flashcards weighted by user score
+
+    def load_and_split(self):
+        """
+        loads pdfs in a folder and splits them into chunks
+        """
+
+        print("Loading pdf...")
+
+        loader = PyPDFDirectoryLoader(self.path)
+        pdfs = loader.load()
+
+        print(f"Loaded {len(pdfs)} documents")
+
+        print("Splitting pdf...")
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 850
+        )
+        chunks = text_splitter.split_documents(pdfs)
+        self.chunks = chunks
+    
+    def generate_flashcards(self,model=mistral_model,amount=1):
+        for i in range(amount):
+
+            index = self.get_new_index() 
+            result = model(self.chunks[index].page_content)
+            question, answer = result.split("#||#")
+            flashcard = (
+                question,  
+                answer,  
+                self.chunks[index].page_content, # context
+                self.chunks[index].metadata.get("page","?") # page
+            )
+            self.cards[index] = flashcard
+            heappush(self.cardheap,(-(100/3),flashcard)) # adds flashcard to queue
+
+    def draw_flashcard(self):
+        """
+        returns next flashcard in queue 
+        """
+        if self.cardheap:
+            return heappop(self.cardheap)
+
+        else:
+            "No flashcards :("
+    
+    def update_card_score(self,card,current_score,userrating):
+        if userrating != 0:
+            new_score = -(100/userrating)
+            updated_score = (new_score*current_score)/2
+            heappush(self.cardheap,(updated_score,card))
+
+
+    
+
+    
+    def show_flashcards(self,card):
+        print(f"Question: {card[0]}")
+        input("Press enter to show answer")
+        print(f"Answer: {card[1]}")
+        ans = input("source? (y/n)")
+        if ans == "y":
+            print(f"Context: {card[2]}")
+            print(f"Page: {card[3]}")
+        print("-----------------------------")
+        userrating = float(input("rate yourself from (1-5) 1 is bad, 5 is good.\n Enter 0 if you don't want to rate: "))
+        return userrating
+
+
+    
+    
+    def get_new_index(self):
+        """
+        finds new index that has not been used 
+        """
+        while True:
+            random_index = randint(0,len(self.chunks)-1)
+            if random_index not in self.cards: 
+                return random_index
+
+
+
+
+def main():
+    deck = FlashcardDeck()
+    deck.load_and_split()
+    while len(deck.cards) < len(deck.chunks): # it will keep generating new cards until all possible are generated
+        deck.generate_flashcards()
+        score,card = deck.draw_flashcard()
+        userrating = deck.show_flashcards(card)
+        deck.update_card_score(card,score,userrating)
+    while True: # just rating system
+        score,card = deck.draw_flashcard()
+        userrating = deck.show_flashcards(card)
+        deck.update_card_score(card,score,userrating)
+    
+
+main()
+
+    
+    
+    
+    
+    
---
+++ models.py
@@ -0,0 +1,68 @@
+from openai import OpenAI
+from mistralai import Mistral
+from config import DEEPSEEK_KEY,MISTRAL_KEY
+
+SYSTEM_CONTENT = """
+Du er ekspert i undervisning.
+
+Generér 1 flashcard (spørgsmål og svar) baseret på et givet tekststykke.
+
+Krav:
+- Stil kun ét spørgsmål, som tester vigtig viden fra teksten.
+- Spørgsmålet må ikke referere til billeder, tabeller eller eksternt materiale.
+- Spørgsmålet skal kunne besvares uden at have teksten foran sig.
+- Svaret må højst være 4 sætninger langt.
+- Omskriv teksten til pædagogisk og letforståeligt sprog – undgå at gentage ordlyden direkte.
+- Giv kun ét flashcard (ét spørgsmål og ét svar).
+- Returnér intet andet end dette: `"Question: [spørgsmål] #||# Answer: [svar]"` (inkluder citationstegn og nøjagtig formatering).
+
+Svarformat:
+"Question: Hvad er fotosyntese? #||# Answer: Fotosyntese er den proces, hvor planter omdanner sollys til energi."
+
+Svar altid i én linje som vist. INGEN forklaringer, mellemrum eller ekstra tekst, og skriv kun Question og Answer en gang.
+"""
+
+
+def deepseek_model(chunk):
+    """
+    uses deepseek model to generate flashcards 
+    """
+    client = OpenAI(
+        api_key = DEEPSEEK_KEY,
+        base_url = "https://api.deepseek.com"
+    )
+    response = client.chat.completions.create(
+        model = "deepseek-chat",
+        messages=[
+            {"role": "system", "content": SYSTEM_CONTENT},
+            {"role": "user", "content": f"TEMATEKST:\n{chunk}"}
+        ]
+    )
+    return response.choices[0].message.content
+    
+def openai_model(chunk):
+    """
+    uses openai model to generate flashcards
+    """
+    client = OpenAI(api_key = "OPENAI_KEY",)
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": SYSTEM_CONTENT},
+            {"role": "user", "content": f"TEMATEKST:\n{chunk}"}
+        ]
+    )
+    return response.choices[0].message.content
+
+
+def mistral_model(chunk):
+    model = "mistral-small-latest"
+    client = Mistral(api_key=MISTRAL_KEY)
+    chat_response = client.chat.complete(
+        model=model,
+        messages = [
+            {"role": "system", "content": SYSTEM_CONTENT},
+            {"role": "user", "content": f"TEMATEKST:\n{chunk}"}
+        ]
+    )
+    return chat_response.choices[0].message.content
